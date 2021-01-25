f7cd89b0c7aa0d076d281b2549ab0d40adeef004
Tal Globus <talglobus@gmail.com>
2021-01-24 19:03:24 -0800

    Start building up basic website

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, redirect, render_template
+import os
 app = Flask(__name__)
+
+root_dir = os.path.dirname(os.getcwd())
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    # print(os.path.join("~/Code/defthedonald/", 'index.html'))
+    return render_template('index.html')
+
 
 @app.route('/api')
 def api():
-    return "You're now in the API!"
+    return "You're now in the API! I am your master and you pickles shall do my bidding!"
+
+
+@app.route('/login')
+def login():
+    return "You can't log in because you're not worthy"
+
+app.run(debug=True)
