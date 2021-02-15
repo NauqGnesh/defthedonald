a466f57e95fd9e730b62679e15634fd66a5d9b1e
Maxim Bravo <maximbravo1@gmail.com>
2021-02-14 19:08:14 -0800

    Clean up code for merging into main

--- model/model_loader.py
+++ model/model_loader.py
@@ -1,15 +1,6 @@
 from transformers import pipeline, AutoTokenizer
-# import gpt_2_simple as gpt2
-# import tensorflow as tf
 
-# sess = None
 pipe = None
-
-def initialize_model():
-    # global sess
-    # sess = gpt2.start_tf_sess()
-    # gpt2.load_gpt2(sess, run_name='run1', checkpoint_dir='model_files')
-    pass
 
 def get_generations(model_folder, num_samples):
     global pipe
@@ -23,25 +14,3 @@ def get_generations(model_folder, num_samples):
     results = pipe([start_token]*num_samples)
     # print(results)
     return [result[0]['generated_text'].replace(start_token, '').replace(end_token, '') for result in results]
-    # global sess
-    # return gpt2.generate(sess,
-    #          run_name='run1',
-    #          checkpoint_dir='model_files', # default 'checkpoint',
-    #          model_name=None,
-    #          model_dir='models',
-    #          sample_dir='samples',
-    #          return_as_list=True, # default False,
-    #          truncate='<|endoftext|>', # default None,
-    #          destination_path=None,
-    #          sample_delim='=' * 20 + '\n',
-    #          prefix='<|startoftext|>', # default None,
-    #          seed=None,
-    #          nsamples=20, # default 1,
-    #          batch_size=20, # default 1,
-    #          length=200, # default 1023,
-    #          temperature=0.7,
-    #          top_k=0,
-    #          top_p=0.0,
-    #          include_prefix=False) # default True)
-
-# President Trump has a new book that is the best. The first book has a very interesting look at the Great New York Times and the Wall Street Journal. His book is called “The New York Times.
--- model/model_main.py
+++ model/model_main.py
@@ -4,15 +4,13 @@ model_app = Flask(__name__)
 
 @model_app.route('/')
 def index():
-    num_samples = 50
+    num_samples = 5
     models_folder = "./model_files/"
     model_name = "10epochs2013-2020"
     # model_name = "3_epochs_distilgpt2_2021-01-25_01-11"
     # model_name = "2_epochs_distilgpt2_2021-01-25_01-11"
     # model_name = "1_epochs_distilgpt2_2021-01-29_21-00"
     generations = model_loader.get_generations(model_folder=models_folder + model_name, num_samples=num_samples)
-
-    # print(generations)
 
     model_samples_folder = "./model_samples/"
     f = open(model_samples_folder + model_name + f"_{str(num_samples)}_examples.txt", "w")
